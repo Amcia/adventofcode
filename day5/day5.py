1cd7a55b802f0031502594cee6e93249137f68e6
amasny <amasny@cisco.com>
2017-12-18 22:57:25 +0100

    commit day5

--- day5/day5.py
+++ day5/day5.py
@@ -1,5 +1,5 @@
 
-def file_handler(file_name):
+def file_handler_1_star(file_name):
     with open(file_name) as fp:
         whole_list = fp.read()
         operation_list = whole_list.split('\n')
@@ -20,5 +20,29 @@ def file_handler(file_name):
         print(count)
 
 
+def file_handler_2_star(file_name):
+    with open(file_name) as fp:
+        whole_list = fp.read()
+        operation_list = whole_list.split('\n')
+        for c, value in enumerate(operation_list):
+            operation_list[c] = int(value)
+        #print(operation_list)
+        count = 0
+        i = 0
+        tmp = 0
+        while True:
+            try:
+                tmp = operation_list[i]
+                if tmp >= 3:
+                    operation_list[i] = tmp - 1
+                else:
+                    operation_list[i] = tmp + 1
+                i += tmp
+                count += 1
+            except IndexError:
+                break
+        print(count)
+
 if __name__ == '__main__':
-    file_handler('input')
+    file_handler_1_star('input')
+    file_handler_2_star('input')
